fdeb2e94ff61cdb6f5a798d64ae78365b7ffae0b
betonit007 <timnagorski@hotmail.com>
2020-07-30 13:21:48 -0400

    added cascading effect for nav anchors/selections

--- script.js
+++ script.js
@@ -15,9 +15,31 @@ function toggleNav() {
         //Animate in
         overlay.classList.remove('overlay-slide-left')
         overlay.classList.add('overlay-slide-right')
+        //Animate in - nav items
+        nav1.classList.remove('slide-out-1')
+        nav1.classList.add('slide-in-1')
+        nav2.classList.remove('slide-out-2')
+        nav2.classList.add('slide-in-2')
+        nav3.classList.remove('slide-out-3')
+        nav3.classList.add('slide-in-3')
+        nav4.classList.remove('slide-out-4')
+        nav4.classList.add('slide-in-4')
+        nav5.classList.remove('slide-out-5')
+        nav5.classList.add('slide-in-5')
     } else {
         overlay.classList.remove('overlay-slide-right')
         overlay.classList.add('overlay-slide-left')
+        //Animate out - nav items
+        nav1.classList.remove('slide-in-1')
+        nav1.classList.add('slide-out-1')
+        nav2.classList.remove('slide-in-2')
+        nav2.classList.add('slide-out-2')
+        nav3.classList.remove('slide-in-3')
+        nav3.classList.add('slide-out-3')
+        nav4.classList.remove('slide-in-4')
+        nav4.classList.add('slide-out-4')
+        nav5.classList.remove('slide-in-5')
+        nav5.classList.add('slide-out-5')
     }
 }
 
@@ -25,6 +47,6 @@ function toggleNav() {
 menuBars.addEventListener('click', toggleNav);
 nav1.addEventListener('click', toggleNav);
 nav2.addEventListener('click', toggleNav);
-nav2.addEventListener('click', toggleNav);
+nav3.addEventListener('click', toggleNav);
 nav4.addEventListener('click', toggleNav);
 nav5.addEventListener('click', toggleNav);
